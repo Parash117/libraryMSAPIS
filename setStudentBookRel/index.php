3296a90ef58bc58cb7ec61adf08e5d677ff17c9e
Parash Gurung <pgrg117@gmail.com>
2020-08-20 22:08:50 +0545

    setStudne and book relation

---
+++ setStudentBookRel/index.php
@@ -0,0 +1,19 @@
+<?php
+  include('../dbconnection.php');
+
+  if(isset($_POST["username"])){
+      $pid = $_POST['pid'];
+      $sid = $_POST['sid'];
+      $dateoflend = date();
+      //fullname,countryname,email,address,gender,username,password,dob_text,phoneno
+      $sql = "INSERT INTO student_book_rel (pid, sid, dateoflend)VALUES('$pid','$sid','$dateoflend')";
+
+      if(mysqli_query($conn, $sql)){
+        echo json_encode(['status'=>'success']);
+      }
+      else{
+        echo json_encode(['status'=>'failed']);
+      }
+    }
+$conn -> close();
+?>
